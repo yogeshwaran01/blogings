f1e0e03a7f55b6bae8769ce7eadabc63d1750889
YOGESHWARAN R <yogeshin247@gmail.com>
2022-02-25 10:05:11 +0530

    updated

--- backend/functions/src/util/admin.ts
+++ backend/functions/src/util/admin.ts
@@ -7,7 +7,7 @@ dotenv.config()
 admin.initializeApp({
     'credential': admin.credential.cert({
         clientEmail: process.env.client_email,
-        privateKey: process.env.project_key,
+        privateKey: process.env.project_key ? JSON.parse(process.env.project_key) : undefined,
         projectId: process.env.project_id
     }),
     'databaseURL': 'market-9c3c5.firebaseio.com'
