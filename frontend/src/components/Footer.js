fc4e9c4e6bc7d6256cea1e8a2dafed1aa49ce5cf
YOGESHWARAN R <yogeshin247@gmail.com>
2022-05-22 09:47:34 +0000

    yeaar updated

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
                 <a className="flex title-font font-medium items-center md:justify-start justify-center text-white" href="/">
                     <span className="ml-3 text-xl">Blogings</span>
                 </a>
-                <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2021 Blogings —
+                <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2022 Blogings —
                     <a href="https://twitter.com/yogeshwaran01" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@yogeshwaran01</a>
                 </p>
             </div>
